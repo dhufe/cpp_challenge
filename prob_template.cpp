d48b01ebaf28b66135360a1e8bf32663dea3ca2b
Daniel Hufschläger <daniel@hufschlaeger.net>
2023-10-11 16:36:06 +0200

    Added a template for future problems.

---
+++ prob_template.cpp
@@ -0,0 +1,26 @@
+/**
+ *       @file  prob_01.cpp
+ *      @brief
+ *
+ *
+ *     @author  Daniel Hufschlaeger (dhufe), daniel@hufschlaeger.net
+ *
+ *   @internal
+ *     Created  11.10.2023
+ *    Revision  ---
+ *    Compiler  gcc/g++
+ *     License  MIT
+ *     Company  dhufe@hufschlaeger.net
+ *   Copyright  Copyright (c) 2023, Daniel Hufschlaeger
+ *
+ *  
+ * 
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+int main ( int argc, const char* argv[] ) {
+ 
+  return 0;
+}
